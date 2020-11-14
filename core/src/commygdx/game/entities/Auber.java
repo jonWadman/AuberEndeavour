be61d5bd20e13220b4f637145bac5d41aa1e5299
Fraser Masson <frasermasson7@gmail.com>
2020-11-14 22:33:52 +0000

    Added MovementSystem, implemented Auber movement

--- core/src/commygdx/game/entities/Auber.java
+++ core/src/commygdx/game/entities/Auber.java
@@ -3,18 +3,40 @@ package commygdx.game.entities;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import commygdx.game.input.PlayerInput;
+import commygdx.game.system.MovementSystem;
 
 public class Auber extends Sprite {
 
     private World world;
     private Body body;
+    private MovementSystem movementSystem;
 
     public Auber(World world){
         this.world = world;
         createBody();
+        movementSystem = new MovementSystem(body,1000);
     }
 
     public void update(float dt){
+        handleMovement();
+        //testing
+        System.out.println(body.getPosition());
+    }
+
+    private void handleMovement(){
+        if(PlayerInput.left()){
+            movementSystem.left();
+        }
+        if(PlayerInput.right()){
+            movementSystem.right();
+        }
+        if(PlayerInput.up()){
+            movementSystem.up();
+        }
+        if(PlayerInput.down()){
+            movementSystem.down();
+        }
     }
 
     private void createBody(){
